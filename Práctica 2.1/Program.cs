f580e5494477ed1a3272bf99c00b128bf8092361
hp <hp@LAPTOP-1KNN72KH>
2022-08-01 18:09:42 -0600

    Agregar archivos de proyecto.

---
+++ Práctica 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Práctica_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int edad = 18;
+            string name = "Vanessa";
+
+            Console.WriteLine("Su nombre es " + name + " y su edad es " + edad);
+        }
+    }
+}
