bc7bae1f0d32236877a6f4050f12b53e2129ce6f
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:52:32 -0600

    Quinto programa/Suma

---
+++ Práctica 2.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Práctica_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese un número:"); 
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número");
+            int num2 = int.Parse(Console.ReadLine());
+            var suma = num1 + num2;
+
+            Console.WriteLine("La suma de los números es : {0}" , suma);
+        }
+    }
+}
