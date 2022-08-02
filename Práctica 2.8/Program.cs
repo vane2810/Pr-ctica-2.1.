4e66f4529dfba33c29c95fd612b627b688d3d3c3
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:57:14 -0600

    Octavo programa/División

---
+++ Práctica 2.8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Práctica_2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número");
+            int num2 = int.Parse(Console.ReadLine());
+            var division = num1 / num2;
+
+            Console.WriteLine("La suma de los números es : {0}", division);
+        }
+    }
+}
