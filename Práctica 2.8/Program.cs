e493516d547834383cbccf07093fd59245c77be5
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:57:45 -0600

    Octavo programa/División

--- Práctica 2.8/Program.cs
+++ Práctica 2.8/Program.cs
@@ -12,7 +12,7 @@ namespace Práctica_2._8
             int num2 = int.Parse(Console.ReadLine());
             var division = num1 / num2;
 
-            Console.WriteLine("La suma de los números es : {0}", division);
+            Console.WriteLine("La división de los números es : {0}", division);
         }
     }
 }
