02b8f0e98e1fd05115d1c73e7750a0b1fbc12cce
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:44:43 -0600

    Cuarto programa

---
+++ Práctica 2.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Práctica_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
