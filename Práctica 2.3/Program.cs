9ae5e86de79f7c6000626811212f385443caaf40
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:35:19 -0600

    Tercer programa

---
+++ Práctica 2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Práctica_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 25.85F;
+            double num2 = 25.85;
+            decimal num3 = 25.85M;
+
+            Console.WriteLine(" {0} {1} {2}" , num1, num2, num3);
+        }
+    }
+}
