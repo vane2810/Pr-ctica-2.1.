ff4173ac324761ccbbcfb2a81f124d56234e7de8
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:28:18 -0600

    Segundo programa

---
+++ Práctica 2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Práctica_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 27;
+            var suma = num1 + num2;
+            int edad = 18;
+            string name = "Vanessa";
+
+            Console.WriteLine(suma);
+            Console.WriteLine("Su nombre es : {1} y su edad es : {0} " , edad , name );
+        }
+    }
+}
