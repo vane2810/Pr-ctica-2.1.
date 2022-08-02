bd03111127614cc0891ec5f6cbca20c9615ccb4b
vane2810 <olgavanessasf@gmail.com>
2022-08-01 22:54:26 -0600

    Sexto programa/Resta

---
+++ Prática 2.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Prática_2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número");
+            int num2 = int.Parse(Console.ReadLine());
+            var resta = num1 - num2;
+
+            Console.WriteLine("La suma de los números es : {0}", resta);
+        
+        }
+    }
+}
